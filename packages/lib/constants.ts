ab7ae4b75b45d935592ecefc445810ac7dbd1237
Kreshnik Abdiju <kreshnik.abdiju@conlabz.de>
2022-06-29 03:27:02 +0200

    add

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -1,7 +1,7 @@
-export const WEBAPP_URL = process.env.NEXT_PUBLIC_WEBAPP_URL || `https://${process.env.VERCEL_URL}`;
+export const WEBAPP_URL = "https://calcomtest.up.railway.app";
 /** @deprecated use `WEBAPP_URL` */
 export const BASE_URL = WEBAPP_URL;
-export const WEBSITE_URL = process.env.NEXT_PUBLIC_WEBSITE_URL || "https://cal.com";
+export const WEBSITE_URL = "https://calcomtest.up.railway.app";
 
 // This is the URL from which all Cal Links and their assets are served.
 // Use website URL to make links shorter(cal.com and not app.cal.com)
@@ -15,6 +15,6 @@ export const CONSOLE_URL =
 export const EMBED_LIB_URL = process.env.NEXT_PUBLIC_EMBED_LIB_URL || `${WEBAPP_URL}/embed/embed.js`;
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 export const TRIAL_LIMIT_DAYS = 14;
-export const HOSTED_CAL_FEATURES = process.env.HOSTED_CAL_FEATURES || BASE_URL === "https://app.cal.com";
+export const HOSTED_CAL_FEATURES = process.env.HOSTED_CAL_FEATURES;
 /** @deprecated use `WEBAPP_URL` */
 export const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_WEBAPP_URL || `https://${process.env.VERCEL_URL}`;
